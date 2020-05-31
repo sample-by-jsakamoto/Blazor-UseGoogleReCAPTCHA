6ed8af5e717d664924af2878bb7e5625c82d2515
jsakamoto <junichi.jp@gmail.com>
2020-05-31 20:27:08 +0900

    Reunion client side sample code

--- BlazorWasmEdition/Client/Program.cs
+++ BlazorWasmEdition/Client/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using BlazorReCaptchaSample.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
